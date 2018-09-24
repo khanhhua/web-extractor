ec560b3d752928271e0612da724f41e9caaf53b5
Thanh Nguyen Ky <thanh.nguyenky@singtel.com>
2018-09-25 01:32:25 +0800

    Add Unittest for ExtractingRouter

---
+++ src/test/java/com/millennium/webextractor/router/ExtractingRouterTest.java
@@ -0,0 +1,31 @@
+package com.millennium.webextractor.router;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@RunWith(SpringRunner.class)
+//  We create a `@SpringBootTest`, starting an actual server on a `RANDOM_PORT`
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ExtractingRouterTest {
+    // Spring Boot will create a `WebTestClient` for you,
+    // already configure and ready to issue requests against "localhost:RANDOM_PORT"
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @Test
+    public void testHello() {
+        webTestClient
+                // Create a GET request to test an endpoint
+                .get().uri("/extract")
+                .accept(MediaType.APPLICATION_JSON_UTF8)
+                .exchange()
+                // and use the dedicated DSL to test assertions against the response
+                .expectStatus().isOk()
+                .expectBody(String.class).isEqualTo("{}");
+    }
+}
