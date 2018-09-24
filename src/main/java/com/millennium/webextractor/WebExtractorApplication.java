58bcb4549dedc42ff572a825edab54ab24eba8ac
Thanh Nguyen Ky <thanh.nguyenky@singtel.com>
2018-09-25 00:48:26 +0800

    initial springboot 2.0.5

---
+++ src/main/java/com/millennium/webextractor/WebExtractorApplication.java
@@ -0,0 +1,12 @@
+package com.millennium.webextractor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebExtractorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebExtractorApplication.class, args);
+	}
+}
